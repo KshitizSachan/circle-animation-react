988831ea17211a561cf0f849f6427cc3f1ca9144
KshitizSachan <kshitizsachan422@gmail.com>
2024-01-21 01:50:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './SphereComponent.css';
 
-function App() {
+const TextSphere = () => {
+  useEffect(() => {
+    const Texts = [
+      'HTML', 'CSS', 'JAVASCRIPT',
+      'jQuery', 'Bootstrap', 'REACT',
+      'Python', 'Java', 'C', 'Javascript',
+      'DJANGO', 'NODEJS', 'MONGODB',
+      'EXPRESS', 'MYSQL', 'JQUERY', 'Tailwind',
+      'Cyber-security', 'Kali-Linux', 'Bug-Bounty'
+    ];
+
+    // Dynamically create a script element to load TagCloud
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/TagCloud@2.2.0/dist/TagCloud.min.js';
+    script.async = true;
+
+    script.onload = () => {
+      // Callback when the script is loaded
+      const tagCloud = window.TagCloud('.Sphere', Texts, {
+        radius: 350,
+        maxSpeed: 'normal',
+        initSpeed: 'fast',
+        direction: 135,
+        keep: true
+      });
+
+      const color = '#FF5733';
+      document.querySelector('.Sphere').style.color = color;
+
+      // Cleanup effect when the component unmounts
+      return () => {
+        tagCloud.destroy();
+      };
+    };
+
+    // Append the script element to the document body
+    document.body.appendChild(script);
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span className="Sphere"></span>
     </div>
   );
-}
+};
 
-export default App;
+export default TextSphere;
